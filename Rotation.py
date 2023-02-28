77b6a86a973df67a1ea44b5142956b8a02fb57db
fertranchida <103848038+fertranchida@users.noreply.github.com>
2023-02-28 12:40:24 +0100

    Update Rotation.py

--- Rotation.py
+++ Rotation.py
@@ -10,7 +10,8 @@ secretsmanager = boto3.client('secretsmanager')
 def lambda_handler(event, context):   
     vsecret = os.getenv('sec')
     secret_list = vsecret.split(';')
-
+    
+    # AK INACTIVATION
     for secret in secret_list:
         get_secret = secretsmanager.get_secret_value(SecretId=secret)
         secret_details = json.loads(get_secret['SecretString'])
@@ -34,7 +35,8 @@ def lambda_handler(event, context):
                     if key_date > key_comp:
                         iam.update_access_key(AccessKeyId=key['AccessKeyId'], Status='Inactive',UserName=key['UserName'])
                         print(key['AccessKeyId'] + " key of " + key['UserName'] + " has been inactivated.")
-
+                        
+    # INACTIVE AK DELETION
     for secret in secret_list:
         get_secret = secretsmanager.get_secret_value(SecretId=secret)
         secret_details = json.loads(get_secret['SecretString'])
@@ -43,35 +45,33 @@ def lambda_handler(event, context):
         
         key_response = iam.list_access_keys(UserName=secret_details['UserName'])
         
-        # Inactive Key will be deleted
         for key in key_response['AccessKeyMetadata']:
             if key['Status'] == 'Inactive':
                 iam.delete_access_key(AccessKeyId=key['AccessKeyId'],UserName=key['UserName'])
                 print("An inactive key - " + key['AccessKeyId'] + ", of " + key['UserName'] + " user has been deleted.")
-
+    
+    # SECRET CREATION
     for secret in secret_list:
         get_secret = secretsmanager.get_secret_value(SecretId=secret)
         secret_details = json.loads(get_secret['SecretString'])
         
         key_response = iam.list_access_keys(UserName=secret_details['UserName'])
 
-        # New Access Key Creation
         create_response = iam.create_access_key(UserName=secret_details['UserName'])
         print("A new set of keys has been created for user - " + secret_details['UserName'])
         
-        # Updating the Secret
         NewSecret = '{"UserName":"' + create_response['AccessKey']['UserName'] + '", "AccessKeyId":"' + create_response['AccessKey']['AccessKeyId'] + '", "SecretAccessKey":"' + create_response['AccessKey']['SecretAccessKey'] + '"}'
         secretsmanager.update_secret(SecretId=secret,SecretString=NewSecret)
         print(secret + " secret has been updated with latest key details for " + secret_details['UserName'] + " user.")
-
+        
+    # AK INACTIVATION
     for secret in secret_list:
         get_secret = secretsmanager.get_secret_value(SecretId=secret)
         secret_details = json.loads(get_secret['SecretString'])
         
-        # Extracting the key details from IAM
         key_response = iam.list_access_keys(UserName=secret_details['UserName'])
         cont2 = 0
-        # Existing Key Inactivation
+
         for key in key_response['AccessKeyMetadata']:
             cont2 = cont2 + 1
             #This counter validates the 2 access key limit from AWS
